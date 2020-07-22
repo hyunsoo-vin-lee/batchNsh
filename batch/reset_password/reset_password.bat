c5096db20c9c5b1fcebe0f4d46c8305f9ae088a1
unknown <hyunsoo.vin.lee@gmail.com>
2020-07-23 08:42:44 +0900

    reset_password.bat 추가

---
+++ batch/reset_password/reset_password.bat
@@ -0,0 +1,5 @@
+c:
+cd ‪C:\Users\HP\Desktop\UserUpdate
+cmd /c D:\R2017x\3DPassport\win_b64\code\command\PassportUserImport.bat -url https://passportdev.hites.co.kr/3dpassport -file passportUser.txt -action update
+
+pause
